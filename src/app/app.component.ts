6f862e4d943050ab209a97f50f6904ec671370a9
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-12-31 13:16:57 -0500

    fixed dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,44 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';  //one way data binding with HTML
+  title = 'todo-list';
   tasks: Task[] = [
     new Task("Visit Ann"),
     new Task("Call Dad"),
     new Task("Go to the gym"),
     new Task("Wash the dishes"),
     new Task("Shop for the party")
-  ];
+  ]
 
   add(newTask: string){
-    this.tasks.push(new Task(newTask));
+    this.tasks.push(new Task(newTask))
   }
 
-  remove(existingTask: Task){
-    var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask}"`)
+  remove(exisitingTask: Task){
+    var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${exisitingTask.title}"`)
+  
     if(userConfirmed){
-      this.tasks = this.tasks.filter(task => task != existingTask)
+      this.tasks = this.tasks.filter(task => task != exisitingTask);
     }
-  }
-
-  markAsDone(task: Task){
-    task.isDone = true;
+  
   }
 }
 
-class Task{
+class Task {
+
   constructor(public title: string){
 
   }
+
+  toggleIsDone(){
+    this.isDone = !this.isDone;
+  }
+
   public isDone = false;
+
 }
